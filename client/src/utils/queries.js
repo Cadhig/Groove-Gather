ecb48ce61c37b4b9f84fcdf4e19fc618d07bc2a0
Rey G. <reyanna.garibay@gmail.com>
2024-08-08 20:49:01 -0500

    query me added

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,22 @@
 import { gql } from "@apollo/client";
 
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
 
 export const GET_TEACHERS = gql`
   query GetTeachers {
